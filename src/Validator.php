46e8de04ac2d4b0a95d0eb84db8769618cdb87a4
Patrick van Loon <p.j.vanloon1991@gmail.com>
2019-06-28 19:36:49 +0200

    Added validator and exceptions.

---
+++ src/Validator.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types = 1);
+
+namespace Aeviiq\ValueObject;
+
+use Aeviiq\ValueObject\Exception\InvalidArgumentException;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+final class Validator
+{
+    /**
+     * @var ValidatorInterface|null
+     */
+    private static $validator;
+
+    /**
+     * @param mixed                   $value
+     * @param Constraint|Constraint[] $rules
+     *
+     * @throws InvalidArgumentException When the value violates any of the given constraints.
+     */
+    public static function validate($value, $constraints): void
+    {
+        $violations = static::getValidator()->validate($value, $constraints);
+        if (0 === $violations->count()) {
+            return;
+        }
+
+        $violationMessages = [];
+        /** @var ConstraintViolation $violation */
+        foreach ($violations as $violation) {
+            $violationMessages[] = $violation->getMessage();
+        }
+
+        throw new InvalidArgumentException(\sprintf('Invalid value: "%s". "%s"', $value, implode('", "', $violationMessages)));
+    }
+
+    private static function getValidator(): ValidatorInterface
+    {
+        if (null === static::$validator) {
+            static::$validator = Validation::createValidator();
+        }
+
+        return static::$validator;
+    }
+}
